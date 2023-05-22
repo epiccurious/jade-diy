7e1c06d00a0b5825e0854a3d7c23a8a1dd271f39
Epic Curious <109078515+epiccurious@users.noreply.github.com>
2023-05-22 16:44:37 -0400

    Use higher-compatibility method for recurse clone

--- flash_your_device.sh
+++ flash_your_device.sh
@@ -22,6 +22,7 @@ sudo apt-get -qq install -y cmake git python3-pip python3-venv
 echo "ok."
 
 esp_git_dir="${HOME}/esp"
+esp_idf_git_tag="v5.0.1"
 jade_git_dir="${HOME}/jade"
 device1="TTGO T-Display"
 device2="M5Stack M5StickC PLUS"
@@ -33,9 +34,9 @@ if [ ! -f "${esp_git_dir}"/esp-idf/export.sh ]
 then
     echo -ne "\n  Downloading the framework... "
     [ -d "${esp_git_dir}" ] || mkdir "${esp_git_dir}"
-    git clone --quiet --recursive https://github.com/espressif/esp-idf.git "${esp_git_dir}"/esp-idf/
+    git clone --quiet https://github.com/espressif/esp-idf.git "${esp_git_dir}"/esp-idf/
     cd "${esp_git_dir}"/esp-idf
-    git checkout v5.0.1 &>/dev/null
+    git checkout --quiet "${esp_idf_git_tag}"
     git submodule update --quiet --init --recursive
     echo "ok."
     echo -n "  Installing the framework... "
@@ -48,11 +49,11 @@ echo -n "Checking for the Blockstream Jade repository... "
 if [ ! -d "${jade_git_dir}" ]
 then
     echo -ne "\n  Downloading Jade... "
-    git clone --quiet --recursive https://github.com/blockstream/jade "${jade_git_dir}"
+    git clone --quiet https://github.com/blockstream/jade.git "${jade_git_dir}"
     #git checkout $(git tag | grep -v miner | sort -V | tail -1)
-    #git submodule update --init --recursive
 fi
 cd "${jade_git_dir}"
+git submodule update --quiet --init --recursive
 echo "ok."
 
 echo -e "\nWhich device do you want to flash?"
@@ -104,8 +105,7 @@ do
     echo
 done
 
-echo "Ready to install Jade on your ${opt}."
-echo "(This process can take over 10 minutes.)"
+echo -e "\nReady to install Jade on your ${opt}.\n(This process can take over 10 minutes.)"
 echo -n "PRESS ANY KEY TO CONTINUE... "
 read -rn1
 echo
